758f0a35d052db96c4676638175765ca52b3fe70
Sapir Malka <sapirm290@gmail.com>
2019-06-24 14:49:52 +0300

    star4 fix

--- OOP2/star4.js
+++ OOP2/star4.js
@@ -1,26 +1,21 @@
-class Plant{
-  constructor(season, color, doesBlossom){
-    this.season = season;
-    this.color = color;
-    this.doesBlossom = doesBlossom;
+class Plant {
+  constructor (name, season, color, doesBlossom) {
+    this.name = name
+    this.season = season
+    this.color = color
+    this.doesBlossom = doesBlossom
   }
 }
-
-
-function getPlantsBySeason(plants, season){
-  var blossomPlants = [];
-  plants.forEach(plant => {
-    if(plant.season === season)blossomPlants.push(plant);
-  });
-  return blossomPlants;
+function getPlantsBySeason (plants, season) {
+  return plants.filter(plant => (plant.season === season && plant.doesBlossom))
 }
-let greenHouse1 = [];
-greenHouse1.push(new Plant("automn", "red", false));
-greenHouse1.push(new Plant("spring", "yellow", true));
-greenHouse1.push(new Plant("winter", "pink", true));
-console.log(getPlantsBySeason(greenHouse1, "spring"));
-let greenHouse2 = [];
-greenHouse2.push(new Plant("spring", "red", false));
-greenHouse2.push(new Plant("spring", "blue", false));
-greenHouse2.push(new Plant("winter", "pink", true));
-console.log(getPlantsBySeason(greenHouse2, "spring"));
+let greenHouse1 = []
+greenHouse1.push(new Plant('rose', 'automn', 'red', false))
+greenHouse1.push(new Plant('aconitum', 'spring', 'yellow', true))
+greenHouse1.push(new Plant('agapanthus', 'winter', 'pink', true))
+console.log(getPlantsBySeason(greenHouse1, 'spring'))
+let greenHouse2 = []
+greenHouse2.push(new Plant('ageratum', 'spring', 'red', true))
+greenHouse2.push(new Plant('alchemilla', 'spring', 'blue', false))
+greenHouse2.push(new Plant('alstroemeria', 'winter', 'pink', true))
+console.log(getPlantsBySeason(greenHouse2, 'spring'))
